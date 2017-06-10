058f77c55c9651f510e4ac93a6fb51f850eb4643
VoltBit <andrei_ciprian.dobre@cti.pub.ro>
2017-06-10 23:37:33 +0300

    daily

--- rust_testing/ellida_daemon/target/debug/deps/ellida_daemon-b9c5143bc11d2317.d
+++
@@ -1,5 +0,0 @@
-/home/smith/Dropbox/ellida/rust_testing/ellida_daemon/target/debug/deps/ellida_daemon-b9c5143bc11d2317: src/main.rs
-
-/home/smith/Dropbox/ellida/rust_testing/ellida_daemon/target/debug/deps/ellida_daemon-b9c5143bc11d2317.d: src/main.rs
-
-src/main.rs:
--- rust_testing/ellida_daemon/target/debug/ellida_daemon.d
+++
@@ -1,1 +0,0 @@
-/home/smith/Dropbox/ellida/rust_testing/ellida_daemon/target/debug/ellida_daemon: /home/smith/Dropbox/ellida/rust_testing/ellida_daemon/src/main.rs
